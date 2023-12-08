4a37b16d55fea1f50740a4bffb353ef6b7c0125a
MazenMostafaa <MazenMostafa047@gmail.com>
2023-12-08 20:56:37 +0200

    fix pathes.

--- src/modules/index.routes.js
+++ src/modules/index.routes.js
@@ -1,7 +1,7 @@
 
 import authRouters from './authModule/auth.routes.js'
 import userRouters from './userModule/user.routes.js'
-import postRouter from '../modules/postModule/post.routes.js'
+import postRouter from './postModule/post.routes.js'
 
 
 export {
