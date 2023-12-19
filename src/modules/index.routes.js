061da2f22e9b4b66c6bfa37ed37c96ebb84c326f
MazenMostafaa <MazenMostafa047@gmail.com>
2023-12-19 05:06:37 +0200

    merge a chat routers to the main index routers file.

--- src/modules/index.routes.js
+++ src/modules/index.routes.js
@@ -2,10 +2,12 @@
 import authRouters from './authModule/auth.routes.js'
 import userRouters from './userModule/user.routes.js'
 import postRouter from './postModule/post.routes.js'
+import chatRouter from './chatModule/chat.routes.js'
 
 
 export {
     authRouters,
     userRouters,
-    postRouter
+    postRouter,
+    chatRouter
 }
